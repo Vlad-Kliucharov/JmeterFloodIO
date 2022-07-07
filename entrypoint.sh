dd6834682e1e73bd17fcc7a621339a49f8f7cabf
Vladyslav <kliucharovvlad@gmail.com>
2022-07-07 18:28:22 +0300

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
-#!/bin/bash
-# Execute JMeter command\
-
+#!/bin/sh
+echo "Execution is being started"
+echo "**************************"
 jmeter $@
-
-#    -n \
-#    -t "/tests/${TEST_DIR}/${TEST_PLAN}.jmx" \
-#    -l "/tests/${TEST_DIR}/${TEST_PLAN}.jtl"
-# exec tail -f jmeter.log
-#    -D "java.rmi.server.hostname=${IP}" \
-#    -D "client.rmi.localport=${RMI_PORT}" \
-#  -R $REMOTE_HOSTS
+echo "**************************"
+echo "Execution has been completed, please check the artifacts to download the results."
